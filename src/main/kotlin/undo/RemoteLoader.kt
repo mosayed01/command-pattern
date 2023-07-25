4c1df85e45ca512099026b428cff47621449607d
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-25 17:58:06 +0300

    comment

--- src/main/kotlin/undo/RemoteLoader.kt
+++ src/main/kotlin/undo/RemoteLoader.kt
@@ -29,25 +29,25 @@ fun main() {
 
     remoteControl.undoButtonWasPushed()
 
-    val ceilingFan = CeilingFan("Living Room")
-
-    val ceilingFanMedium = CeilingFanMediumCommand(ceilingFan)
-    val ceilingFanHigh = CeilingFanHighCommand(ceilingFan)
-    val ceilingFanOff = CeilingFanOffCommand(ceilingFan)
-
-    remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff)
-    remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff)
-
-    remoteControl.onButtonWasPushed(0)
-    remoteControl.offButtonWasPushed(0)
-
-    println(remoteControl)
-
-    remoteControl.undoButtonWasPushed()
-
-    remoteControl.onButtonWasPushed(1)
-
-    println(remoteControl)
-
-    remoteControl.undoButtonWasPushed()
+//    val ceilingFan = CeilingFan("Living Room")
+//
+//    val ceilingFanMedium = CeilingFanMediumCommand(ceilingFan)
+//    val ceilingFanHigh = CeilingFanHighCommand(ceilingFan)
+//    val ceilingFanOff = CeilingFanOffCommand(ceilingFan)
+//
+//    remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff)
+//    remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff)
+//
+//    remoteControl.onButtonWasPushed(0)
+//    remoteControl.offButtonWasPushed(0)
+//
+//    println(remoteControl)
+//
+//    remoteControl.undoButtonWasPushed()
+//
+//    remoteControl.onButtonWasPushed(1)
+//
+//    println(remoteControl)
+//
+//    remoteControl.undoButtonWasPushed()
 }
